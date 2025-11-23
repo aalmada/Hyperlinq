625e16a2cc4d135646e5dce54aeaad7e320765d0
Antão Almada <antao.almada@gmail.com>
2025-11-23 00:53:48 +0000

    refactor: replace multiple `Sum` overloads with a single generic `INumber<T>` implementation.

--- NetFabric.Hyperlinq/Optimized.cs
+++ NetFabric.Hyperlinq/Optimized.cs
@@ -60,57 +60,16 @@ namespace NetFabric.Hyperlinq
         public static WhereEnumerable<TSource> Where<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
             => new WhereEnumerable<TSource>(source, predicate);
 
-        public static int Sum(this IEnumerable<int> source)
+        public static T Sum<T>(this IEnumerable<T> source)
+            where T : System.Numerics.INumber<T>
         {
-            if (source is int[] array)
-                return (int)System.Numerics.Tensors.TensorPrimitives.Sum<int>(array);
+            if (source is T[] array)
+                return T.CreateChecked(System.Numerics.Tensors.TensorPrimitives.Sum<T>(array));
 
-            if (source is List<int> list)
-                return (int)System.Numerics.Tensors.TensorPrimitives.Sum(System.Runtime.InteropServices.CollectionsMarshal.AsSpan(list));
+            if (source is List<T> list)
+                return T.CreateChecked(System.Numerics.Tensors.TensorPrimitives.Sum(System.Runtime.InteropServices.CollectionsMarshal.AsSpan(list)));
 
-            var sum = 0;
-            foreach (var item in source)
-                sum += item;
-            return sum;
-        }
-
-        public static long Sum(this IEnumerable<long> source)
-        {
-            if (source is long[] array)
-                return (long)System.Numerics.Tensors.TensorPrimitives.Sum<long>(array);
-
-            if (source is List<long> list)
-                return (long)System.Numerics.Tensors.TensorPrimitives.Sum(System.Runtime.InteropServices.CollectionsMarshal.AsSpan(list));
-
-            var sum = 0L;
-            foreach (var item in source)
-                sum += item;
-            return sum;
-        }
-
-        public static float Sum(this IEnumerable<float> source)
-        {
-            if (source is float[] array)
-                return System.Numerics.Tensors.TensorPrimitives.Sum(array);
-
-            if (source is List<float> list)
-                return System.Numerics.Tensors.TensorPrimitives.Sum(System.Runtime.InteropServices.CollectionsMarshal.AsSpan(list));
-
-            var sum = 0f;
-            foreach (var item in source)
-                sum += item;
-            return sum;
-        }
-
-        public static double Sum(this IEnumerable<double> source)
-        {
-            if (source is double[] array)
-                return System.Numerics.Tensors.TensorPrimitives.Sum(array);
-
-            if (source is List<double> list)
-                return System.Numerics.Tensors.TensorPrimitives.Sum(System.Runtime.InteropServices.CollectionsMarshal.AsSpan(list));
-
-            var sum = 0.0;
+            var sum = T.Zero;
             foreach (var item in source)
                 sum += item;
             return sum;
