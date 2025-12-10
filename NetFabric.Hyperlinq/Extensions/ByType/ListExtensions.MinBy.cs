eb366d6fa9d451301f78c4682e2276e72ecf5ed4
Antão Almada <antao.almada@gmail.com>
2025-12-10 16:08:20 +0000

    feat: add MinBy, MaxBy, and MinMaxBy methods for various collection types.

---
+++ NetFabric.Hyperlinq/Extensions/ByType/ListExtensions.MinBy.cs
@@ -0,0 +1,115 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace NetFabric.Hyperlinq
+{
+    public static partial class ListExtensions
+    {
+        extension<TSource>(List<TSource> source)
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MinBy<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinBy(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MinBy<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinBy<TSource, TKey, TSelector>(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MinBy<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinBy<TSource, TKey, TSelector>(in selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MinByOrNone<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinByOrNone(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MinByOrNone<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinByOrNone<TSource, TKey, TSelector>(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MinByOrNone<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinByOrNone<TSource, TKey, TSelector>(in selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MaxBy<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MaxBy(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MaxBy<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MaxBy<TSource, TKey, TSelector>(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MaxBy<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MaxBy<TSource, TKey, TSelector>(in selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MaxByOrNone<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MaxByOrNone(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MaxByOrNone<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MaxByOrNone<TSource, TKey, TSelector>(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MaxByOrNone<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MaxByOrNone<TSource, TKey, TSelector>(in selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public (TSource Min, TSource Max) MinMaxBy<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinMaxBy(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public (TSource Min, TSource Max) MinMaxBy<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinMaxBy<TSource, TKey, TSelector>(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public (TSource Min, TSource Max) MinMaxBy<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinMaxBy<TSource, TKey, TSelector>(in selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<(TSource Min, TSource Max)> MinMaxByOrNone<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinMaxByOrNone(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<(TSource Min, TSource Max)> MinMaxByOrNone<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinMaxByOrNone<TSource, TKey, TSelector>(selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<(TSource Min, TSource Max)> MinMaxByOrNone<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => CollectionsMarshal.AsSpan(source).MinMaxByOrNone<TSource, TKey, TSelector>(in selector);
+        }
+    }
+}
---
+++ NetFabric.Hyperlinq/Extensions/ByType/ReadOnlySpanExtensions.MinBy.cs
@@ -0,0 +1,272 @@
+using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace NetFabric.Hyperlinq
+{
+    public static partial class ReadOnlySpanExtensions
+    {
+        extension<TSource>(ReadOnlySpan<TSource> source)
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MinBy<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => source.MinByOrNone(selector).Value;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MinBy<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => source.MinByOrNone<TSource, TKey, TSelector>(selector).Value;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MinBy<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => source.MinByOrNone<TSource, TKey, TSelector>(selector).Value;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MinByOrNone<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => MinByOrNoneImpl<TSource, TKey, FunctionWrapper<TSource, TKey>>(source, new FunctionWrapper<TSource, TKey>(selector));
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MinByOrNone<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => MinByOrNoneImpl<TSource, TKey, TSelector>(source, selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MinByOrNone<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => MinByOrNoneInImpl<TSource, TKey, TSelector>(source, selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MaxBy<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => source.MaxByOrNone(selector).Value;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MaxBy<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => source.MaxByOrNone<TSource, TKey, TSelector>(selector).Value;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public TSource MaxBy<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => source.MaxByOrNone<TSource, TKey, TSelector>(selector).Value;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MaxByOrNone<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => MaxByOrNoneImpl<TSource, TKey, FunctionWrapper<TSource, TKey>>(source, new FunctionWrapper<TSource, TKey>(selector));
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MaxByOrNone<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => MaxByOrNoneImpl<TSource, TKey, TSelector>(source, selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<TSource> MaxByOrNone<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => MaxByOrNoneInImpl<TSource, TKey, TSelector>(source, selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public (TSource Min, TSource Max) MinMaxBy<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => source.MinMaxByOrNone(selector).Value;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public (TSource Min, TSource Max) MinMaxBy<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => source.MinMaxByOrNone<TSource, TKey, TSelector>(selector).Value;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public (TSource Min, TSource Max) MinMaxBy<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => source.MinMaxByOrNone<TSource, TKey, TSelector>(selector).Value;
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<(TSource Min, TSource Max)> MinMaxByOrNone<TKey>(Func<TSource, TKey> selector)
+                where TKey : IComparable<TKey>
+                => MinMaxByOrNoneImpl<TSource, TKey, FunctionWrapper<TSource, TKey>>(source, new FunctionWrapper<TSource, TKey>(selector));
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<(TSource Min, TSource Max)> MinMaxByOrNone<TKey, TSelector>(TSelector selector)
+                where TSelector : struct, IFunction<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => MinMaxByOrNoneImpl<TSource, TKey, TSelector>(source, selector);
+
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            public Option<(TSource Min, TSource Max)> MinMaxByOrNone<TKey, TSelector>(in TSelector selector)
+                where TSelector : struct, IFunctionIn<TSource, TKey>
+                where TKey : IComparable<TKey>
+                => MinMaxByOrNoneInImpl<TSource, TKey, TSelector>(source, selector);
+        }
+
+        static Option<TSource> MinByOrNoneImpl<TSource, TKey, TSelector>(ReadOnlySpan<TSource> source, TSelector selector)
+            where TSelector : struct, IFunction<TSource, TKey>
+            where TKey : IComparable<TKey>
+        {
+            if (source.Length == 0)
+                return Option<TSource>.None();
+
+            var min = source[0];
+            var minKey = selector.Invoke(min);
+
+            for (var index = 1; index < source.Length; index++)
+            {
+                var item = source[index];
+                var key = selector.Invoke(item);
+                if (key.CompareTo(minKey) < 0)
+                {
+                    min = item;
+                    minKey = key;
+                }
+            }
+
+            return Option<TSource>.Some(min);
+        }
+
+        static Option<TSource> MinByOrNoneInImpl<TSource, TKey, TSelector>(ReadOnlySpan<TSource> source, TSelector selector)
+            where TSelector : struct, IFunctionIn<TSource, TKey>
+            where TKey : IComparable<TKey>
+        {
+            if (source.Length == 0)
+                return Option<TSource>.None();
+
+            var min = source[0];
+            var minKey = selector.Invoke(in min);
+
+            for (var index = 1; index < source.Length; index++)
+            {
+                ref readonly var item = ref source[index];
+                var key = selector.Invoke(in item);
+                if (key.CompareTo(minKey) < 0)
+                {
+                    min = item;
+                    minKey = key;
+                }
+            }
+
+            return Option<TSource>.Some(min);
+        }
+
+        static Option<TSource> MaxByOrNoneImpl<TSource, TKey, TSelector>(ReadOnlySpan<TSource> source, TSelector selector)
+            where TSelector : struct, IFunction<TSource, TKey>
+            where TKey : IComparable<TKey>
+        {
+            if (source.Length == 0)
+                return Option<TSource>.None();
+
+            var max = source[0];
+            var maxKey = selector.Invoke(max);
+
+            for (var index = 1; index < source.Length; index++)
+            {
+                var item = source[index];
+                var key = selector.Invoke(item);
+                if (key.CompareTo(maxKey) > 0)
+                {
+                    max = item;
+                    maxKey = key;
+                }
+            }
+
+            return Option<TSource>.Some(max);
+        }
+
+        static Option<TSource> MaxByOrNoneInImpl<TSource, TKey, TSelector>(ReadOnlySpan<TSource> source, TSelector selector)
+            where TSelector : struct, IFunctionIn<TSource, TKey>
+            where TKey : IComparable<TKey>
+        {
+            if (source.Length == 0)
+                return Option<TSource>.None();
+
+            var max = source[0];
+            var maxKey = selector.Invoke(in max);
+
+            for (var index = 1; index < source.Length; index++)
+            {
+                ref readonly var item = ref source[index];
+                var key = selector.Invoke(in item);
+                if (key.CompareTo(maxKey) > 0)
+                {
+                    max = item;
+                    maxKey = key;
+                }
+            }
+
+            return Option<TSource>.Some(max);
+        }
+
+        static Option<(TSource Min, TSource Max)> MinMaxByOrNoneImpl<TSource, TKey, TSelector>(ReadOnlySpan<TSource> source, TSelector selector)
+            where TSelector : struct, IFunction<TSource, TKey>
+            where TKey : IComparable<TKey>
+        {
+            if (source.Length == 0)
+                return Option<(TSource Min, TSource Max)>.None();
+
+            var min = source[0];
+            var minKey = selector.Invoke(min);
+            var max = min;
+            var maxKey = minKey;
+
+            for (var index = 1; index < source.Length; index++)
+            {
+                var item = source[index];
+                var key = selector.Invoke(item);
+                if (key.CompareTo(minKey) < 0)
+                {
+                    min = item;
+                    minKey = key;
+                }
+                else if (key.CompareTo(maxKey) > 0)
+                {
+                    max = item;
+                    maxKey = key;
+                }
+            }
+
+            return Option<(TSource Min, TSource Max)>.Some((min, max));
+        }
+
+        static Option<(TSource Min, TSource Max)> MinMaxByOrNoneInImpl<TSource, TKey, TSelector>(ReadOnlySpan<TSource> source, TSelector selector)
+            where TSelector : struct, IFunctionIn<TSource, TKey>
+            where TKey : IComparable<TKey>
+        {
+            if (source.Length == 0)
+                return Option<(TSource Min, TSource Max)>.None();
+
+            var min = source[0];
+            var minKey = selector.Invoke(in min);
+            var max = min;
+            var maxKey = minKey;
+
+            for (var index = 1; index < source.Length; index++)
+            {
+                ref readonly var item = ref source[index];
+                var key = selector.Invoke(in item);
+                if (key.CompareTo(minKey) < 0)
+                {
+                    min = item;
+                    minKey = key;
+                }
+                else if (key.CompareTo(maxKey) > 0)
+                {
+                    max = item;
+                    maxKey = key;
+                }
+            }
+
+            return Option<(TSource Min, TSource Max)>.Some((min, max));
+        }
+    }
+}
